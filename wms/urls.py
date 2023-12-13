9c256844ecff6aef8180d0fd15fd8ce3fe463799
jijiluzana <133578376+jijiluzana@users.noreply.github.com>
2023-12-13 10:37:50 +0800

    23

--- wms/urls.py
+++ wms/urls.py
@@ -2,11 +2,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.views.static import serve
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('warehouse.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
